45bca218b39feb882a868693a1889287ddcfd64b
amman.edwin <amman.edwin777@gmail.com>
2024-03-24 23:55:21 +0500

    push my BMI calculator

---
+++ index.ts
@@ -0,0 +1,16 @@
+// BMI calculator using inquirer prompt
+
+import inquirer from "inquirer";
+const question = [
+    {message:"what is your weight(KG)",
+type:"number",name:"weight"},
+{message:"what is your height(M)",
+type:"number",name:"height"},
+];
+const calculateBMI = (weight: number, height: number): number =>weight /(height*height)
+const main = async() =>{
+    const {weight,height} =await inquirer.prompt(question);
+    console.log (`your BMI is : ${calculateBMI(weight,height).toFixed(2)}`);
+}
+
+main();
